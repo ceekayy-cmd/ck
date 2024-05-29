c1c639e8b84fdda4ae7294219c3170dc2da8bc42
ceekayy-cmd <129663981+ceekayy-cmd@users.noreply.github.com>
2024-05-29 20:43:12 +0800

    Update assesment.sol

--- assesment.sol
+++ assesment.sol
@@ -1,29 +1,74 @@
 pragma solidity ^0.8.13;
 
-contract ErrorManagement {
-
-    function validateInput(uint value) public pure {
-        require(value > 10, "Value must be greater than 10");
+contract LocalStore {
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        uint stock;
     }
 
-    function verifyCondition(uint value) public pure {
-        if (value <= 10) {
-            revert("Value must be greater than 10");
+    mapping(uint => Product) public products;
+    uint public productCount;
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    event ProductAdded(uint id, string name, uint price, uint stock);
+    event ProductPurchased(uint id, uint quantity, address buyer);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addProduct(string memory _name, uint _price, uint _stock) public onlyOwner {
+        require(bytes(_name).length > 0, "Product name required");
+        require(_price > 0, "Product price must be greater than 0");
+        require(_stock > 0, "Product stock must be greater than 0");
+
+        productCount++;
+        products[productCount] = Product(productCount, _name, _price, _stock);
+
+        emit ProductAdded(productCount, _name, _price, _stock);
+    }
+
+    function purchaseProduct(uint _id, uint _quantity) public payable {
+        Product storage product = products[_id];
+        require(product.id != 0, "Product does not exist");
+        require(product.stock >= _quantity, "Not enough stock available");
+        require(msg.value >= product.price * _quantity, "Not enough ether sent");
+
+        if (product.stock < _quantity) {
+            revert("Not enough stock available");
         }
+        if (msg.value < product.price * _quantity) {
+            revert("Not enough ether sent");
+        }
+
+        product.stock -= _quantity;
+        payable(owner).transfer(msg.value);
+
+        emit ProductPurchased(_id, _quantity, msg.sender);
     }
 
-    uint public count;
+    function getProduct(uint _id) public view returns (string memory, uint, uint) {
+        Product memory product = products[_id];
+        require(product.id != 0, "Product does not exist");
 
-    function checkInvariant() public view {
-        assert(count == 0);
+        return (product.name, product.price, product.stock);
     }
 
-    error LowBalance(uint availableBalance, uint requestedAmount);
-
-    function checkBalance(uint withdrawAmount) public view {
+    function withdrawFunds(uint _amount) public onlyOwner {
         uint balance = address(this).balance;
-        if (balance < withdrawAmount) {
-            revert LowBalance({availableBalance: balance, requestedAmount: withdrawAmount});
-        }
+        assert(balance >= _amount);
+        require(_amount <= balance, "Insufficient balance");
+        payable(owner).transfer(_amount);
+    }
+
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
     }
 }
