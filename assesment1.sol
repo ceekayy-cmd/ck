00f07e191be5d10d50bbc12ca63c224c70d92e53
ceekayy-cmd <129663981+ceekayy-cmd@users.noreply.github.com>
2024-05-31 23:43:23 +0800

    Update assesment1.sol

--- assesment1.sol
+++ assesment1.sol
@@ -21,7 +21,7 @@ contract PlayerRegistry {
     }
 
     function _randomModulus(uint256 _modulus) private view returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, players.length))) % _modulus;
+        return uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, players.length))) % _modulus;
     }
 
     function showplayers() public view returns (string[] memory Players) {
